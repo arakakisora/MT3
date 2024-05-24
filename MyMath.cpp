c032787ea52b6e18d20528921fce8095edfa70ea
arakakisora <k023g0045@g.neec.ac.jp>
2024-05-24 16:39:05 +0900

    プロジェクト ファイルを追加します。

---
+++ MyMath.cpp
@@ -0,0 +1,418 @@
+﻿#include "MyMath.h"
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
+{
+	Matrix4x4 ans;
+
+	ans.m[0][0] = 1;
+	ans.m[0][1] = 0;
+	ans.m[0][2] = 0;
+	ans.m[0][3] = 0;
+
+	ans.m[1][0] = 0;
+	ans.m[1][1] = 1;
+	ans.m[1][2] = 0;
+	ans.m[1][3] = 0;
+
+	ans.m[2][0] = 0;
+	ans.m[2][1] = 0;
+	ans.m[2][2] = 1;
+	ans.m[2][3] = 0;
+
+	ans.m[3][0] = translate.x;
+	ans.m[3][1] = translate.y;
+	ans.m[3][2] = translate.z;
+	ans.m[3][3] = 1;
+
+	return ans;
+
+}
+
+Matrix4x4 MakeScaleMatrix(const Vector3& scale)
+{
+	Matrix4x4 ans;
+
+	ans.m[0][0] = scale.x;
+	ans.m[0][1] = 0;
+	ans.m[0][2] = 0;
+	ans.m[0][3] = 0;
+
+	ans.m[1][0] = 0;
+	ans.m[1][1] = scale.y;
+	ans.m[1][2] = 0;
+	ans.m[1][3] = 0;
+
+	ans.m[2][0] = 0;
+	ans.m[2][1] = 0;
+	ans.m[2][2] = scale.z;
+	ans.m[2][3] = 0;
+
+	ans.m[3][0] = 0;
+	ans.m[3][1] = 0;
+	ans.m[3][2] = 0;
+	ans.m[3][3] = 1;
+	return ans;
+
+}
+
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix)
+{
+
+	Vector3 ans;
+
+	ans.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	ans.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	ans.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+
+	assert(w != 0.0f);
+	ans.x /= w;
+	ans.y /= w;
+	ans.z /= w;
+	return ans;
+
+}
+
+Matrix4x4 MakeRotateXMatrix(float radian)
+{
+
+	Matrix4x4 ans;
+	ans.m[0][0] = 1;
+	ans.m[0][1] = 0;
+	ans.m[0][2] = 0;
+	ans.m[0][3] = 0;
+
+	ans.m[1][0] = 0;
+	ans.m[1][1] = std::cos(radian);;
+	ans.m[1][2] = std::sin(radian);;
+	ans.m[1][3] = 0;
+
+	ans.m[2][0] = 0;
+	ans.m[2][1] = -std::sin(radian);;
+	ans.m[2][2] = std::cos(radian);;
+	ans.m[2][3] = 0;
+
+	ans.m[3][0] = 0;
+	ans.m[3][1] = 0;
+	ans.m[3][2] = 0;
+	ans.m[3][3] = 1;
+
+	return ans;
+
+}
+
+Matrix4x4 MakeRotateYMatrix(float radian)
+{
+
+	Matrix4x4 ans;
+	ans.m[0][0] = std::cos(radian);
+	ans.m[0][1] = 0;
+	ans.m[0][2] = -std::sin(radian);
+	ans.m[0][3] = 0;
+
+	ans.m[1][0] = 0;
+	ans.m[1][1] = 1;
+	ans.m[1][2] = 0;
+	ans.m[1][3] = 0;
+
+	ans.m[2][0] = std::sin(radian);;
+	ans.m[2][1] = 0;
+	ans.m[2][2] = std::cos(radian);;
+	ans.m[2][3] = 0;
+
+	ans.m[3][0] = 0;
+	ans.m[3][1] = 0;
+	ans.m[3][2] = 0;
+	ans.m[3][3] = 1;
+
+	return ans;
+
+}
+
+Matrix4x4 MakeRotateZMatrix(float radian)
+{
+	Matrix4x4 ans;
+	ans.m[0][0] = std::cos(radian);
+	ans.m[0][1] = std::sin(radian);
+	ans.m[0][2] = 0;
+	ans.m[0][3] = 0;
+
+	ans.m[1][0] = -std::sin(radian);
+	ans.m[1][1] = std::cos(radian);
+	ans.m[1][2] = 0;
+	ans.m[1][3] = 0;
+
+	ans.m[2][0] = 0;
+	ans.m[2][1] = 0;
+	ans.m[2][2] = 1;
+	ans.m[2][3] = 0;
+
+	ans.m[3][0] = 0;
+	ans.m[3][1] = 0;
+	ans.m[3][2] = 0;
+	ans.m[3][3] = 1;
+
+
+	return ans;
+
+}
+
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate)
+{
+
+	Matrix4x4 rotateXYZ = Multiply(Multiply(MakeRotateXMatrix(rotate.x), MakeRotateYMatrix(rotate.y)), MakeRotateZMatrix(rotate.z));
+	return Multiply(Multiply(MakeScaleMatrix(scale), rotateXYZ), MakeTranslateMatrix(translate));
+
+}
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label)
+{
+
+	static const int kColumanWidth = 60;
+
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumanWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumanWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumanWidth * 3, y, "%s", label);
+
+
+}
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix)
+{
+
+	static const int krowHeight = 20;
+	static const int kcolumnWidth = 60;
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kcolumnWidth, y + row * krowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
+
+Matrix4x4 Add(const Matrix4x4& mt1, const Matrix4x4& mt2)
+{
+
+	Matrix4x4 ans;
+
+	ans.m[0][0] = mt1.m[0][0] + mt2.m[0][0];
+	ans.m[0][1] = mt1.m[0][1] + mt2.m[0][1];
+	ans.m[0][2] = mt1.m[0][2] + mt2.m[0][2];
+	ans.m[0][3] = mt1.m[0][3] + mt2.m[0][3];
+
+	ans.m[1][0] = mt1.m[1][0] + mt2.m[1][0];
+	ans.m[1][1] = mt1.m[1][1] + mt2.m[1][1];
+	ans.m[1][2] = mt1.m[1][2] + mt2.m[1][2];
+	ans.m[1][3] = mt1.m[1][3] + mt2.m[1][3];
+
+	ans.m[2][0] = mt1.m[2][0] + mt2.m[2][0];
+	ans.m[2][1] = mt1.m[2][1] + mt2.m[2][1];
+	ans.m[2][2] = mt1.m[2][2] + mt2.m[2][2];
+	ans.m[2][3] = mt1.m[2][3] + mt2.m[2][3];
+
+	ans.m[3][0] = mt1.m[3][0] + mt2.m[3][0];
+	ans.m[3][1] = mt1.m[3][1] + mt2.m[3][1];
+	ans.m[3][2] = mt1.m[3][2] + mt2.m[3][2];
+	ans.m[3][3] = mt1.m[3][3] + mt2.m[3][3];
+
+
+
+	return ans;
+
+
+}
+
+Matrix4x4 Subtract(const Matrix4x4& mt1, const Matrix4x4& mt2)
+{
+
+	Matrix4x4 ans;
+
+	ans.m[0][0] = mt1.m[0][0] - mt2.m[0][0];
+	ans.m[0][1] = mt1.m[0][1] - mt2.m[0][1];
+	ans.m[0][2] = mt1.m[0][2] - mt2.m[0][2];
+	ans.m[0][3] = mt1.m[0][3] - mt2.m[0][3];
+
+	ans.m[1][0] = mt1.m[1][0] - mt2.m[1][0];
+	ans.m[1][1] = mt1.m[1][1] - mt2.m[1][1];
+	ans.m[1][2] = mt1.m[1][2] - mt2.m[1][2];
+	ans.m[1][3] = mt1.m[1][3] - mt2.m[1][3];
+
+	ans.m[2][0] = mt1.m[2][0] - mt2.m[2][0];
+	ans.m[2][1] = mt1.m[2][1] - mt2.m[2][1];
+	ans.m[2][2] = mt1.m[2][2] - mt2.m[2][2];
+	ans.m[2][3] = mt1.m[2][3] - mt2.m[2][3];
+
+	ans.m[3][0] = mt1.m[3][0] - mt2.m[3][0];
+	ans.m[3][1] = mt1.m[3][1] - mt2.m[3][1];
+	ans.m[3][2] = mt1.m[3][2] - mt2.m[3][2];
+	ans.m[3][3] = mt1.m[3][3] - mt2.m[3][3];
+
+
+
+	return ans;
+
+
+}
+
+Matrix4x4 Multiply(const Matrix4x4& mt1, const Matrix4x4& mt2)
+{
+
+	Matrix4x4 ans = {};
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
+
+				ans.m[i][j] += mt1.m[i][k] * mt2.m[k][j];
+
+			}
+
+		}
+
+	}
+	return ans;
+
+}
+
+Matrix4x4 Inverse(const Matrix4x4& mt1)
+{
+	Matrix4x4 ans;
+	float A = 0;
+	A = mt1.m[0][0] * mt1.m[1][1] * mt1.m[2][2] * mt1.m[3][3] + mt1.m[0][0] * mt1.m[1][2] * mt1.m[2][3] * mt1.m[3][1] + mt1.m[0][0] * mt1.m[1][3] * mt1.m[2][1] * mt1.m[3][2]
+		- mt1.m[0][0] * mt1.m[1][3] * mt1.m[2][2] * mt1.m[3][1] - mt1.m[0][0] * mt1.m[1][2] * mt1.m[2][1] * mt1.m[3][3] - mt1.m[0][0] * mt1.m[1][1] * mt1.m[2][3] * mt1.m[3][2]
+		- mt1.m[0][1] * mt1.m[1][0] * mt1.m[2][2] * mt1.m[3][3] - mt1.m[0][2] * mt1.m[1][0] * mt1.m[2][3] * mt1.m[3][1] - mt1.m[0][3] * mt1.m[1][0] * mt1.m[2][1] * mt1.m[3][2]
+		+ mt1.m[0][3] * mt1.m[1][0] * mt1.m[2][2] * mt1.m[3][1] + mt1.m[0][2] * mt1.m[1][0] * mt1.m[2][1] * mt1.m[3][3] + mt1.m[0][1] * mt1.m[1][0] * mt1.m[2][3] * mt1.m[3][2]
+		+ mt1.m[0][1] * mt1.m[1][2] * mt1.m[2][0] * mt1.m[2][2] + mt1.m[0][2] * mt1.m[1][3] * mt1.m[2][0] * mt1.m[3][1] + mt1.m[0][3] * mt1.m[1][1] * mt1.m[2][0] * mt1.m[3][2]
+		- mt1.m[0][3] * mt1.m[1][2] * mt1.m[2][0] * mt1.m[3][1] - mt1.m[0][2] * mt1.m[1][1] * mt1.m[2][0] * mt1.m[3][3] - mt1.m[0][1] * mt1.m[1][3] * mt1.m[2][0] * mt1.m[3][2]
+		- mt1.m[0][1] * mt1.m[1][2] * mt1.m[2][3] * mt1.m[3][0] - mt1.m[0][2] * mt1.m[1][3] * mt1.m[2][1] * mt1.m[3][0] - mt1.m[0][3] * mt1.m[1][1] * mt1.m[2][2] * mt1.m[3][0]
+		+ mt1.m[0][3] * mt1.m[1][2] * mt1.m[2][0] * mt1.m[3][0] + mt1.m[0][2] * mt1.m[1][1] * mt1.m[2][3] * mt1.m[3][0] + mt1.m[0][1] * mt1.m[1][3] * mt1.m[2][2] * mt1.m[3][0];
+
+
+
+	ans.m[0][0] = (mt1.m[1][1] * mt1.m[2][2] * mt1.m[3][3] + mt1.m[1][2] * mt1.m[2][3] * mt1.m[3][1] + mt1.m[1][3] * mt1.m[2][1] * mt1.m[3][2]
+		- mt1.m[1][3] * mt1.m[2][2] * mt1.m[3][1] - mt1.m[1][2] * mt1.m[2][1] * mt1.m[3][3] - mt1.m[1][1] * mt1.m[2][3] * mt1.m[3][2]) / A;
+
+	ans.m[0][1] = (-mt1.m[0][1] * mt1.m[2][2] * mt1.m[3][3] - mt1.m[0][2] * mt1.m[2][3] * mt1.m[3][1] - mt1.m[0][3] * mt1.m[2][1] * mt1.m[3][2]
+		+ mt1.m[0][3] * mt1.m[2][2] * mt1.m[3][1] + mt1.m[0][2] * mt1.m[2][1] * mt1.m[3][3] + mt1.m[0][1] * mt1.m[2][3] * mt1.m[3][2]) / A;
+
+	ans.m[0][2] = (mt1.m[0][1] * mt1.m[1][2] * mt1.m[3][3] + mt1.m[0][2] * mt1.m[1][3] * mt1.m[3][1] + mt1.m[0][3] * mt1.m[1][1] * mt1.m[3][2]
+		- mt1.m[0][3] * mt1.m[1][2] * mt1.m[3][1] - mt1.m[0][2] * mt1.m[1][1] * mt1.m[3][3] - mt1.m[0][1] * mt1.m[1][3] * mt1.m[3][2]) / A;
+
+	ans.m[0][3] = (-mt1.m[0][1] * mt1.m[1][2] * mt1.m[2][3] - mt1.m[0][2] * mt1.m[1][3] * mt1.m[2][1] - mt1.m[0][3] * mt1.m[1][1] * mt1.m[2][2]
+		+ mt1.m[0][3] * mt1.m[1][2] * mt1.m[2][1] + mt1.m[0][2] * mt1.m[1][1] * mt1.m[2][3] + mt1.m[0][1] * mt1.m[1][3] * mt1.m[2][2]) / A;
+
+
+	ans.m[1][0] = (-mt1.m[1][0] * mt1.m[2][2] * mt1.m[3][3] - mt1.m[1][2] * mt1.m[2][3] * mt1.m[3][0] - mt1.m[1][3] * mt1.m[2][0] * mt1.m[3][2]
+		+ mt1.m[1][3] * mt1.m[2][2] * mt1.m[3][0] + mt1.m[1][2] * mt1.m[2][0] * mt1.m[3][3] + mt1.m[1][0] * mt1.m[2][3] * mt1.m[3][2]) / A;
+
+	ans.m[1][1] = (mt1.m[0][0] * mt1.m[2][2] * mt1.m[3][3] + mt1.m[0][2] * mt1.m[2][3] * mt1.m[3][0] + mt1.m[0][3] * mt1.m[2][0] * mt1.m[3][2]
+		- mt1.m[0][3] * mt1.m[2][2] * mt1.m[3][0] - mt1.m[0][2] * mt1.m[2][0] * mt1.m[3][3] - mt1.m[0][0] * mt1.m[2][3] * mt1.m[3][2]) / A;
+
+	ans.m[1][2] = (-mt1.m[0][0] * mt1.m[1][2] * mt1.m[3][3] - mt1.m[0][2] * mt1.m[1][3] * mt1.m[3][0] - mt1.m[0][3] * mt1.m[1][0] * mt1.m[3][2]
+		+ mt1.m[0][3] * mt1.m[1][2] * mt1.m[3][0] + mt1.m[0][2] * mt1.m[1][0] * mt1.m[3][3] + mt1.m[0][0] * mt1.m[1][3] * mt1.m[3][2]) / A;
+
+	ans.m[1][3] = (mt1.m[0][0] * mt1.m[1][2] * mt1.m[2][3] + mt1.m[0][2] * mt1.m[1][3] * mt1.m[2][0] + mt1.m[0][3] * mt1.m[1][0] * mt1.m[2][2]
+		- mt1.m[0][3] * mt1.m[1][2] * mt1.m[2][0] - mt1.m[0][2] * mt1.m[1][0] * mt1.m[2][3] - mt1.m[0][0] * mt1.m[1][3] * mt1.m[2][2]) / A;
+
+
+
+	ans.m[2][0] = (mt1.m[1][0] * mt1.m[2][1] * mt1.m[3][3] + mt1.m[1][1] * mt1.m[2][3] * mt1.m[3][0] + mt1.m[1][3] * mt1.m[2][0] * mt1.m[3][1]
+		- mt1.m[1][3] * mt1.m[2][1] * mt1.m[3][0] - mt1.m[1][1] * mt1.m[2][0] * mt1.m[3][3] - mt1.m[1][0] * mt1.m[2][3] * mt1.m[3][1]) / A;
+
+	ans.m[2][1] = (-mt1.m[0][0] * mt1.m[2][1] * mt1.m[3][3] - mt1.m[0][1] * mt1.m[2][3] * mt1.m[3][0] - mt1.m[0][3] * mt1.m[2][0] * mt1.m[3][1]
+		+ mt1.m[0][3] * mt1.m[2][1] * mt1.m[3][0] + mt1.m[0][1] * mt1.m[2][0] * mt1.m[3][3] + mt1.m[0][0] * mt1.m[2][3] * mt1.m[3][1]) / A;
+
+	ans.m[2][2] = (mt1.m[0][0] * mt1.m[1][1] * mt1.m[3][3] + mt1.m[0][1] * mt1.m[1][3] * mt1.m[3][0] + mt1.m[0][3] * mt1.m[1][0] * mt1.m[3][1]
+		- mt1.m[0][3] * mt1.m[1][1] * mt1.m[3][0] - mt1.m[0][1] * mt1.m[1][0] * mt1.m[3][3] - mt1.m[0][0] * mt1.m[1][3] * mt1.m[3][1]) / A;
+
+	ans.m[2][3] = (-mt1.m[0][0] * mt1.m[1][1] * mt1.m[2][3] - mt1.m[0][1] * mt1.m[1][3] * mt1.m[2][0] - mt1.m[0][3] * mt1.m[1][0] * mt1.m[2][1]
+		+ mt1.m[0][3] * mt1.m[1][1] * mt1.m[2][0] + mt1.m[0][1] * mt1.m[1][0] * mt1.m[2][3] + mt1.m[0][0] * mt1.m[1][3] * mt1.m[2][1]) / A;
+
+
+	ans.m[3][0] = (-mt1.m[1][0] * mt1.m[2][1] * mt1.m[3][2] - mt1.m[1][1] * mt1.m[2][2] * mt1.m[3][0] - mt1.m[1][2] * mt1.m[2][0] * mt1.m[3][1]
+		+ mt1.m[1][2] * mt1.m[2][1] * mt1.m[3][0] + mt1.m[1][1] * mt1.m[2][0] * mt1.m[3][2] + mt1.m[1][0] * mt1.m[2][2] * mt1.m[3][1]) / A;
+
+	ans.m[3][1] = (mt1.m[0][0] * mt1.m[2][1] * mt1.m[3][2] + mt1.m[0][1] * mt1.m[2][2] * mt1.m[3][0] + mt1.m[0][2] * mt1.m[2][0] * mt1.m[3][1]
+		- mt1.m[0][2] * mt1.m[2][1] * mt1.m[3][0] - mt1.m[0][1] * mt1.m[2][0] * mt1.m[3][2] - mt1.m[0][0] * mt1.m[2][2] * mt1.m[3][1]) / A;
+
+	ans.m[3][2] = (-mt1.m[0][0] * mt1.m[1][1] * mt1.m[3][2] - mt1.m[0][1] * mt1.m[1][2] * mt1.m[3][0] - mt1.m[0][2] * mt1.m[1][0] * mt1.m[3][1]
+		+ mt1.m[0][2] * mt1.m[1][1] * mt1.m[3][0] + mt1.m[0][1] * mt1.m[1][0] * mt1.m[3][2] + mt1.m[0][0] * mt1.m[1][2] * mt1.m[3][1]) / A;
+
+	ans.m[3][3] = (mt1.m[0][0] * mt1.m[1][1] * mt1.m[2][2] + mt1.m[0][1] * mt1.m[1][2] * mt1.m[2][0] + mt1.m[0][2] * mt1.m[1][0] * mt1.m[2][1]
+		- mt1.m[0][2] * mt1.m[1][1] * mt1.m[2][0] - mt1.m[0][1] * mt1.m[1][0] * mt1.m[2][2] - mt1.m[0][0] * mt1.m[1][2] * mt1.m[2][1]) / A;
+
+	return ans;
+
+}
+
+Matrix4x4 Transpose(const Matrix4x4& mt1)
+{
+
+	Matrix4x4 ans;
+
+
+	ans.m[0][0] = mt1.m[0][0];
+	ans.m[0][1] = mt1.m[1][0];
+	ans.m[0][2] = mt1.m[2][0];
+	ans.m[0][3] = mt1.m[3][0];
+
+	ans.m[1][0] = mt1.m[0][1];
+	ans.m[1][1] = mt1.m[1][1];
+	ans.m[1][2] = mt1.m[2][1];
+	ans.m[1][3] = mt1.m[3][1];
+
+	ans.m[2][0] = mt1.m[0][2];
+	ans.m[2][1] = mt1.m[1][2];
+	ans.m[2][2] = mt1.m[2][2];
+	ans.m[2][3] = mt1.m[3][2];
+
+	ans.m[3][0] = mt1.m[0][3];
+	ans.m[3][1] = mt1.m[1][3];
+	ans.m[3][2] = mt1.m[2][3];
+	ans.m[3][3] = mt1.m[3][3];
+
+
+	return ans;
+
+}
+
+Matrix4x4 MekeIdentity4x4()
+{
+
+	Matrix4x4 ans;
+
+
+	ans.m[0][0] = 1;
+	ans.m[0][1] = 0;
+	ans.m[0][2] = 0;
+	ans.m[0][3] = 0;
+
+	ans.m[1][0] = 0;
+	ans.m[1][1] = 1;
+	ans.m[1][2] = 0;
+	ans.m[1][3] = 0;
+
+	ans.m[2][0] = 0;
+	ans.m[2][1] = 0;
+	ans.m[2][2] = 1;
+	ans.m[2][3] = 0;
+
+	ans.m[3][0] = 0;
+	ans.m[3][1] = 0;
+	ans.m[3][2] = 0;
+	ans.m[3][3] = 1;
+
+	return ans;
+
+}
+
+Vector3 Cross(const Vector3& v1, const Vector3& v2)
+{
+	Vector3 ans;
+
+	ans.x = v1.y * v2.z - v1.z * v2.y;
+	ans.y = v1.z * v2.x - v1.x * v2.z;
+	ans.z = v1.x * v2.y - v1.y * v2.x;
+
+	return ans;
+}
---
+++ MyMath.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include "Matrix4x4.h"
+#include "Vector3.h"
+#pragma once
+#include <assert.h>
+#include <Novice.h>
+#include <cmath>
+#include <stdio.h>
+
+
+//回転
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+//拡大
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+//同時座標変換
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+
+//回転X
+Matrix4x4 MakeRotateXMatrix(float radian);
+//回転Y
+Matrix4x4 MakeRotateYMatrix(float radian);
+//回転Z
+Matrix4x4 MakeRotateZMatrix(float radian);
+
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
+void  VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
+
+//スクリーンプリントf
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix);
+
+//足し算
+Matrix4x4 Add(const Matrix4x4& mt1, const Matrix4x4& mt2);
+//引き算
+Matrix4x4 Subtract(const Matrix4x4& mt1, const Matrix4x4& mt2);
+
+//掛け算
+Matrix4x4 Multiply(const Matrix4x4& mt1, const Matrix4x4& mt2);
+
+//逆行列
+Matrix4x4 Inverse(const Matrix4x4& mt1);
+
+
+Matrix4x4 Transpose(const Matrix4x4& mt1);
+//0行列
+Matrix4x4 MekeIdentity4x4();
+
+//クロス積
+Vector3 Cross(const Vector3& v1, const Vector3& v2);
